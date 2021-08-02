3fc7536a7a48dab9c0468028b3d747828848d80a
Hasitha-Su <Hasithakms@gmail.com>
2021-08-02 07:25:58 +0530

    update .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DustbinModule } from './Dustbin/dustbin.module';
-import { MONGODB_CONNECTION } from './app.properties'
 
 @Module({
   imports: [
     DustbinModule,
-    MongooseModule.forRoot(MONGODB_CONNECTION)
+    MongooseModule.forRoot(`${process.env.MONGODB_CONNECTION}`)
   ],
   controllers: [],
   providers: [],
